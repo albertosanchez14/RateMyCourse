4855d1fe04fa3920eecc1a6fb7e480345b7cc48b
Alberto Sanchez del Alamo <alberto.sanchez.delalamo@gmail.com>
2025-05-20 20:26:58 +0200

    d

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -138,11 +138,11 @@ export default function LoginForm({
 
   const handleGoogleSignIn = async () => {
     try {
-      alert('ENV: ' + import.meta.env.VITE_ENV);
       const redirectBaseUrl = 
         import.meta.env.VITE_ENV === "production"
           ? import.meta.env.VITE_URL_PROD
           : import.meta.env.VITE_URL_DEV;
+      console.error("Redirect Base URL:", redirectBaseUrl);
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
